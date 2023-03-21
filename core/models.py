6792746fb6d7ee95a4f0e96ae333faf833de0281
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-21 16:47:36 -0400

    created Author Model and added to Book Model

--- core/models.py
+++ core/models.py
@@ -6,3 +6,25 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Book(models.Model):
+    CHOICES = (
+        ('education', 'Education'),
+        ('graphic novel', 'Graphic Novel'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('thriller', 'Thriller'),
+    )
+
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        to='Author',
+        on_delete=models.CASCADE,
+    )
+    date_published = models.DateField(blank=True, null=True)
+    genre = models.CharField(choices=CHOICES, max_length=50)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
