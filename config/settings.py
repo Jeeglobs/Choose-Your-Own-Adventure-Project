1849019ef861f97fbacc8fad6b7545297154591f
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-21 16:01:14 -0400

    created User Model and ran migrations

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'core',
 
     'rest_framework',
 ]
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
