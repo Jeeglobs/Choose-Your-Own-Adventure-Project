e7b9bffaeb3b389404a019bfce814af3ca7f9af7
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-24 15:35:17 -0400

    added '' to default in settings

--- config/settings.py
+++ config/settings.py
@@ -102,9 +102,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        default: env.db()
-    }
+    'default': env.db()
 }
 
 
