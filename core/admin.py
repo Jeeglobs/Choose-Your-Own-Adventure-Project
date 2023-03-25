b0140613f5a181975ae9c4c0b02a06551b3ffcb6
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-25 12:11:04 -0400

    added FeaturedBook to admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Book, Author, Tracker
+from .models import User, Book, Author, Tracker, FeaturedBook
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Tracker)
+admin.site.register(FeaturedBook)
